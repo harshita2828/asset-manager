25e14d2cc7876756a31f7e0f23ed6b53637882b9
HARSHITA <93899633+harshita2828@users.noreply.github.com>
2025-04-24 22:28:31 +0530

    added logger in categoryServiceImpl

--- src/main/java/com/example/ams/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/example/ams/service/impl/CategoryServiceImpl.java
@@ -5,6 +5,8 @@ import com.example.ams.entities.Category;
 import com.example.ams.form.request.CategoryRequestDTO;
 import com.example.ams.form.response.CategoryResponseDTO;
 import com.example.ams.service.CategoryService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
@@ -12,6 +14,8 @@ import java.util.stream.Collectors;
 
 @Service
 public class CategoryServiceImpl implements CategoryService {
+
+    private static final Logger logger = LoggerFactory.getLogger(CategoryServiceImpl.class);
     private final CategoryRepository categoryRepository;
 
     public CategoryServiceImpl(CategoryRepository categoryRepository) {
@@ -24,11 +28,15 @@ public class CategoryServiceImpl implements CategoryService {
             if (categoryRequestDTO == null ||
                     categoryRequestDTO.getName() == null || categoryRequestDTO.getName().trim().isEmpty() ||
                     categoryRequestDTO.getDescription() == null || categoryRequestDTO.getDescription().trim().isEmpty()) {
+                logger.error("Name and description are required.");
                 throw new IllegalArgumentException("Invalid request: Name and Description are required.");
             }
 
             Optional<Category> existingCategory = categoryRepository.findByName(categoryRequestDTO.getName().trim());
+            logger.info("Category retrieved with name: " + categoryRequestDTO.getName());
+
             if(existingCategory.isPresent()){
+                logger.error("Category already exists with name: " + categoryRequestDTO.getName());
                 throw new RuntimeException("Category with this name already exists.");
             }
 
@@ -37,7 +45,7 @@ public class CategoryServiceImpl implements CategoryService {
             category.setDescription(categoryRequestDTO.getDescription().trim());
 
             Category savedCategory = categoryRepository.save(category);
-            System.out.println("Saved Category: " + savedCategory.getName());
+            logger.info("Category saved with id: " + savedCategory.getId());
 
             return new CategoryResponseDTO(
                     savedCategory.getId().toString(),
@@ -45,6 +53,7 @@ public class CategoryServiceImpl implements CategoryService {
                     savedCategory.getDescription()
             );
         } catch (Exception e) {
+            logger.error("Error while saving category: " + e.getMessage());
             throw new RuntimeException("Error saving category: " + e.getMessage());
         }
     }
@@ -53,8 +62,9 @@ public class CategoryServiceImpl implements CategoryService {
     public List<CategoryResponseDTO> getAllCategories() {
         try {
             List<Category> categories = categoryRepository.findAll();
-
+            logger.info("Categories retrieved.");
             if (categories.isEmpty()) {
+                logger.error("No categories in db.");
                 throw new RuntimeException("No categories found.");
             }
 
@@ -66,6 +76,7 @@ public class CategoryServiceImpl implements CategoryService {
                     ))
                     .collect(Collectors.toList());
         } catch (Exception e) {
+            logger.info("Error while fetching categories: " + e.getMessage());
             throw new RuntimeException("Error fetching categories: " + e.getMessage());
         }
     }
@@ -74,10 +85,13 @@ public class CategoryServiceImpl implements CategoryService {
     public void deleteCategory(String id) {
         try {
             if (!categoryRepository.existsById(Long.valueOf(id))) {
+                logger.error("Category not found with id: " + id);
                 throw new RuntimeException("Category not found.");
             }
             categoryRepository.deleteById(Long.valueOf(id));
+            logger.info("Category deleted with id: " + id);
         } catch (Exception e) {
+            logger.info("Error while deleting category: " + e.getMessage());
             throw new RuntimeException("Error deleting category: " + e.getMessage());
         }
     }
@@ -94,6 +108,7 @@ public class CategoryServiceImpl implements CategoryService {
                     category.getDescription()
             );
         } catch (Exception e) {
+            logger.error("Error while fetching category: " + e.getMessage());
             throw new RuntimeException("Error fetching category : " + e.getMessage());
         }
     }
@@ -104,6 +119,7 @@ public class CategoryServiceImpl implements CategoryService {
             if(categoryRequestDTO == null ||
             categoryRequestDTO.getName() == null || categoryRequestDTO.getName().trim().isEmpty() ||
             categoryRequestDTO.getDescription() == null || categoryRequestDTO.getDescription().trim().isEmpty()) {
+                logger.error("Name and description required for updating category.");
                 throw new IllegalArgumentException("Invalid request: Name and Description are required.");
             }
 
@@ -114,6 +130,7 @@ public class CategoryServiceImpl implements CategoryService {
             category.setDescription(categoryRequestDTO.getDescription().trim());
 
             Category updatedCategory = categoryRepository.save(category);
+            logger.info("Category updated with id: " + id);
 
             return new CategoryResponseDTO(
                     updatedCategory.getId().toString(),
@@ -121,6 +138,7 @@ public class CategoryServiceImpl implements CategoryService {
                     updatedCategory.getDescription()
             );
         } catch (Exception e) {
+            logger.error("Error while updating category: " + e.getMessage());
             throw new RuntimeException("Error updating category: " + e.getMessage());
         }
     }
